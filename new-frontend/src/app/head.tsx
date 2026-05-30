dd06ff28991ac2aac56b940d0625b4154a37a802
aice18 <ayushkvhy927@gmail.com>
2026-05-30 17:50:36 +0530

    update next.config.ts

--- new-frontend/src/app/head.tsx
+++ new-frontend/src/app/head.tsx
@@ -3,12 +3,6 @@ export default function Head() {
     <>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
-        rel="stylesheet"
-      />
     </>
   );
 }
